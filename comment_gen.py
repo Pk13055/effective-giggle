2c024c3352aaa61a7d5dd5b8f21a83ebb86a986c
pk13055 <pkrockstar7@gmail.com>
2017-04-01 13:18:42 +0530

    comment generator updated

--- comment_gen.py
+++ comment_gen.py
@@ -6,9 +6,20 @@ from flask_sqlalchemy import SQLAlchemy
 from app import db
 from app.models import Comment, Problem, User
 
+
+users = User.query.with_entities(User.uid).all()
+user_list = []
+for x in users:
+	user_list.append(x.uid)
+
+problems = Problem.query.with_entities(Problem.uid).all()
+problem_list = []
+for x in problems:
+	problem_list.append(x.uid)
+
 for _ in range(int(sys.argv[1])):
-	user_id = randint(1, 10 ** 5)
-	problem_id = randint(1, 10 ** 5)
+	user_id = user_list[randint(0, len(user_list) - 1)]
+	problem_id = problem_list[randint(0, len(problem_list) - 1)]
 	body = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(randint(10, 200)))
 	db.session.add(Comment(user_id, problem_id, body))
 
