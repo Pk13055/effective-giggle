fd8d6eb30c8753753d4f025bb11791924e5b51a5
pk13055 <pkrockstar7@gmail.com>
2017-04-02 15:06:26 +0530

    Dir structure refreshed

--- problem_gen.py
+++ problem_gen.py
@@ -19,15 +19,7 @@ for x in uploaders:
 
 
 for _ in range(int(sys.argv[1])):
-	# 	def __init__(self, title, tags, uploader,  problem_location, io_location, 
-	# solution_language, solution_location, editorial_location):
 	title = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase +  string.digits) for _ in range(randint(10, 50)))
-	# tag_line = ""
-	# for j in range(randint(0,10)):
-	# 	tag = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase   + string.digits) for _ in range(randint(4,10)))
-	# 	tag_line = tag_line + tag + ","
-	# tag_line = tag_line[:len(tag_line) - 1]
-	# tags = tag_line
 	tag=tags[0]+','+tags[2]+','+tags[1]
 	uploader = admins[randint(0, len(admins) - 1)]
 	problem_location = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase   + string.digits) for _ in range(5)) + '/' + ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(randint(4, 10))) + '/' + ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(randint(12,45)))
