c4e4170f4bf358b3f34c5f14ca1f1fc07340c868
pk13055 <pkrockstar7@gmail.com>
2017-04-03 14:51:25 +0530

    Comments section cleaned up

--- app/Comments/controllers.py
+++ app/Comments/controllers.py
@@ -1,11 +1,10 @@
+
+# def imports required for routing etc
 from flask import Blueprint, request, render_template, \
                   flash, g, session, redirect, url_for, jsonify
 from app import db, models
-from helper import *
+import helper
 
-from selenium import webdriver
-import urllib
-import urllib2
 
 comments = Blueprint('comments', __name__)
 
@@ -13,17 +12,10 @@ comments = Blueprint('comments', __name__)
 @comments.route('/comments/<code>', methods = ['GET', 'POST'])
 def comments_render(code):
 	if request.method == 'GET':
-		comments = models.Comment.query.filter(models.Comment.problem_id == code).with_entities(Comment.user_id).all()
-		comment_obj = []
-		for comment in comments:
-			user = models.User.filter(User.uid == comment.user_id).all()
-			user = user[0]
-			comment_obj.append({ 'name' : user.username, 'profile_pic' : user.profile_pic, 
-				'timestamp' : comment.comment_timestamp, 'body' : comment.body })
-		driver = webdriver.Firefox()
-		driver.get(driver.getCurrentUrl())
-		logged_in = 34
-		return render_template('Main/comments.html', title = code, comments = comment_obj, user_id = logged_in)
+		comment_obj = helper.getComments(code)
+		title = helper.getData(code)
+		return render_template('Main/comments.html', title = title['title'], comments = comment_obj, editorial = title['editorial_location'])
+		# return jsonify(comment_obj)
 	elif request.method == 'POST':
-		addStudent(request.form, code)
-		redirect('/comments/' + str(code))
+		helper.addComment(request.form, code)
+		return redirect(url_for('comments.comments_render', code = code))
--- app/Comments/helper.py
+++ app/Comments/helper.py
@@ -1,14 +1,49 @@
 # define the functions to be used alongside routes here
 # This modular design will help the routes from becoming too cluttered
 
-from app.models import Comment
-from app import db
+from flask import jsonify
+from app.models import Comment, User
+from app import db, models
+import config
 
-def addComment(form, problem_id):
+# custom imports 
+import os
+
+# this function adds the comment to the db 
+def addComment(form, code):
 	user_id = form['user_id']
 	text = form['body']
 	try:
-		db.session.add(Comment(user_id, problem_id, text))
-		db.session.commit()
+		if user_id == "":
+			raise Exception
+		else:
+			db.session.add(Comment(user_id, code, text))
+			db.session.commit()
+		return True
 	except:
-		return False
+		return None
+
+
+# this gets you the comments in the form of a JSON-esque object
+# each member contains the name, profile_pic, timestamp, body
+def getComments(code):
+	comments = Comment.query.join(User, User.uid == Comment.user_id).add_columns(User.username, User.profile_location, Comment.comment_timestamp, Comment.body).filter(Comment.problem_id == code).order_by(Comment.id.desc()).all()
+	comment_list = []
+	for comment in comments:
+		comment_list.append({
+				'name' : comment.username,
+				'profile_pic' : os.path.join(config.UPLOAD_FOLDER_IMAGE, comment.profile_location),
+				'timestamp' : comment.comment_timestamp[:-7],
+				'body' : comment.body
+			})
+	return comment_list
+
+# this gets you the title of the problem and the editorial location
+# modify to make editorial support markdown as well
+def getData(code):
+	data = models.Problem.query.filter(models.Problem.uid == code).with_entities(models.Problem.title, models.Problem.editorial_location).first()
+	try:
+		body = open(os.path.join(config.UPLOAD_FOLDER_EDITORIAL, data.editorial_location)).read()
+	except:
+		body = "No Editorial available for this problem"
+	return {'title' : data.title, 'editorial_location' : body }
