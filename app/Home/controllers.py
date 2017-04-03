21e4423deefae7a49b0e77c4564bbb407436c18d
pk13055 <pkrockstar7@gmail.com>
2017-04-03 11:17:29 +0530

    Cleaned/modularized home controllers

--- app/Home/controllers.py
+++ app/Home/controllers.py
@@ -1,11 +1,9 @@
 import os
 from flask import Blueprint, request, render_template, \
                   flash, g, session, redirect, url_for, jsonify
+
 from app import db, models,requires_auth 
 import helper
-from sqlalchemy.exc import IntegrityError
-from werkzeug.utils import secure_filename
-from app.models import User
 
 home = Blueprint('home', __name__)
 
@@ -19,24 +17,10 @@ def signin():
 	if request.method == 'GET':
 		return render_template('Forms/loginpage.html')
 	elif request.method == 'POST':
-		try:
-			email = request.form['email']
-			password=request.form['password']
-			
-		except KeyError as e:
-			return jsonify(success=False,message="%s doesnt exist")% e.args,400
-
-		user=User.query.filter(User.email==email).first()
-		if user is None:
-			return jsonify(success=False,message="Register First"),401
-
-		elif not user.check_password_hash(password):
-			return jsonify(success=False,message="Wrong Password"),401
-		session['user_uid']=user.uid
-		return jsonify(redirect='/solver/'+user.uid)
+		return helper.signInUser(request)
 
 
-@home.route('/logout',methods=['POST','GET'])
+@home.route('/logout',methods = ['POST','GET'])
 @requires_auth
 def logout():
 	session.pop('user_uid')
@@ -47,23 +31,4 @@ def signup():
 	if request.method == 'GET':
 		return render_template('Forms/registration.html')
 	elif request.method == 'POST':
-		try:
-			email = request.form["email"]
-			institute = request.form["institute"]
-			location = request.form["location"]
-			password = request.form["password"]
-			role = request.form["role"]
-			username = request.form["username"]
-			picpath = request.form["picpath"]
-			profile_location = username + picpath
-
-		except KeyError as e:
-			return jsonify(success=False, message="%s not sent in the request" % e.args), 400 
-
-		u = User(username, email, password, institute, location, role, profile_location)
-		db.session.add(u)
-		try:
-			db.session.commit()
-		except IntegrityError as e:
-			return jsonify(success=False, message="This email already exists"), 400		
-		return render_template('Forms/loginpage.html')
+		return helper.createUser(request)
--- app/Home/helper.py
+++ app/Home/helper.py
@@ -3,6 +3,11 @@
 from app import db, models
 from flask import jsonify
 
+# helper modules required
+from sqlalchemy.exc import IntegrityError
+from werkzeug.utils import secure_filename
+from app.models import User
+
 
 def makeJSON(email, username, password):
 		role = "admin"
@@ -20,3 +25,40 @@ def makeNewUser(form):
 		if x != 'submit' and x != 'password_2':
 			obj[x] = form[x]
 	return jsonify(obj)
+
+
+def signInUser(request):
+	try:
+		email = request.form['email']
+		password = request.form['password']
+	except KeyError as e:
+		return jsonify(success = False, message = " %s doesnt exist") % e.args, 400
+	user = User.query.filter(User.email == email).first()
+	if user is None:
+		return jsonify(success = False, message = "Register First"), 401
+	elif not user.check_password_hash(password):
+		return jsonify(success = False, message = "Wrong Password"), 401
+	session['user_uid'] = user.uid
+	return jsonify(redirect = '/solver/' + user.uid)
+
+def createUser(request):
+	try:
+		email = request.form["email"]
+		institute = request.form["institute"]
+		location = request.form["location"]
+		password = request.form["password"]
+		role = request.form["role"]
+		username = request.form["username"]
+		picpath = request.form["picpath"]
+		profile_location = username + picpath
+
+	except KeyError as e:
+		return jsonify(success=False, message="%s not sent in the request" % e.args), 400 
+
+	u = User(username, email, password, institute, location, role, profile_location)
+	db.session.add(u)
+	try:
+		db.session.commit()
+	except IntegrityError as e:
+		return jsonify(success=False, message="This email already exists"), 400		
+	return render_template('Forms/loginpage.html')	
